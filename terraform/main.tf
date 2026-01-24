4bf3b5e6e0bb8e914aedf95d891022649ab2d682
koyo129 <kohyo0129@gmail.com>
2026-01-24 16:25:19 +0900

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,67 @@
 
 
 Add Terraform directory structure
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+# Get latest Amazon Linux 2023 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
+# Security Group: allow HTTP from anywhere (for now)
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP inbound"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "All outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Use default VPC to keep it simple first
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+# EC2 instance running Nginx via user_data
+resource "aws_instance" "web" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t3.micro"
+  subnet_id              = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = file("../scripts/user_data.sh")
+
+  tags = {
+    Name = "tf-linux-nginx"
+  }
+}
