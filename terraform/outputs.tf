33fe93600008342c51296945fc2655a089b534a9
koyo129 <kohyo0129@gmail.com>
2026-01-25 00:46:01 +0900

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,10 @@ output "web_public_ip" {
 output "web_public_dns" {
   value = aws_instance.web.public_dns
 }
+output "instance_public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+output "instance_public_dns" {
+  value = aws_instance.web.public_dns
+}
