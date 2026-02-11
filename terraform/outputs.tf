8214d7ca82451a76af8c8b450e881f5b8d23a8f4
Kohyo Miyajima <kohyo0129@gmail.com>
2026-02-11 22:05:05 +0900

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,3 @@
-output "web_public_ip" {
-  value = aws_instance.web.public_ip
-}
-
-output "web_public_dns" {
-  value = aws_instance.web.public_dns
-}
-output "instance_public_ip" {
-  value = aws_instance.web.public_ip
-}
-
-output "instance_public_dns" {
-  value = aws_instance.web.public_dns
+output "alb_dns_name" {
+  value = aws_lb.app.dns_name
 }
