1154b2099d0ba2bf3abe41143557b5478d5c6505
Kohyo Miyajima <kohyo0129@gmail.com>
2026-02-14 22:32:53 +0900

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,15 @@
-
 variable "aws_region" {
-  type    = string
-  default = "ap-northeast-1"
+  type = string
 }
 
 variable "vpc_cidr" {
-  type    = string
-  default = "10.0.0.0/16"
+  type = string
 }
 
 variable "public_subnet_cidrs" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+  type = list(string)
 }
 
 variable "private_subnet_cidrs" {
-  type    = list(string)
-  default = ["10.0.101.0/24", "10.0.102.0/24"]
+  type = list(string)
 }
