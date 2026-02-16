d98e2c3d02acdac96776c8e73f7d5a0d0dd98139
Kohyo Miyajima <kohyo0129@gmail.com>
2026-02-16 22:56:18 +0900

    Create logging.tf

---
+++ terraform/logging.tf
@@ -0,0 +1,52 @@
+
+# S3 Bucket for ALB Logs
+
+resource "aws_s3_bucket" "alb_logs" {
+  bucket = "tf-alb-logs-${random_id.suffix.hex}"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket_ownership_controls" "alb_logs" {
+  bucket = aws_s3_bucket.alb_logs.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "alb_logs" {
+  depends_on = [aws_s3_bucket_ownership_controls.alb_logs]
+  bucket     = aws_s3_bucket.alb_logs.id
+  acl        = "private"
+}
+
+
+data "aws_elb_service_account" "main" {}
+
+resource "aws_s3_bucket_policy" "alb_logs" {
+  bucket = aws_s3_bucket.alb_logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = data.aws_elb_service_account.main.arn
+        }
+        Action   = "s3:PutObject"
+        Resource = "${aws_s3_bucket.alb_logs.arn}/*"
+      }
+    ]
+  })
+}
+
+
+access_logs {
+    bucket  = aws_s3_bucket.alb_logs.id
+    prefix  = "alb-logs"
+    enabled = true
+  }
