0e51d551fa717f4e8e8627303dd3770b33ea8e67
Kohyo Miyajima <kohyo0129@gmail.com>
2026-02-18 22:34:06 +0900

    Create alb.tf

---
+++ terraform/alb.tf
@@ -0,0 +1,39 @@
+# ALB + Target Group
+########################
+
+resource "aws_lb" "app" {
+  name               = "tf-alb"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public[0].id, aws_subnet.public[1].id]
+
+  tags = { Name = "tf-alb" }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "tf-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
