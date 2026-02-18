35bbd29f81ad66dd316278e0fc7de4ea3e0f7c35
Kohyo Miyajima <kohyo0129@gmail.com>
2026-02-18 22:33:41 +0900

    Create security.tf

---
+++ terraform/security.tf
@@ -0,0 +1,48 @@
+# Security Groups
+########################
+
+# ALB SG: allow HTTP from the internet
+resource "aws_security_group" "alb_sg" {
+  name        = "tf-alb-sg"
+  description = "ALB allows HTTP from internet"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "HTTP from internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "All outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 SG: allow HTTP ONLY from ALB SG
+resource "aws_security_group" "ec2_sg" {
+  name        = "tf-ec2-sg"
+  description = "EC2 allows HTTP only from ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description     = "HTTP from ALB"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    description = "All outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
